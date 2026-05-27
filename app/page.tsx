c76581f668c29f1eb07d6b4a70f87297d2fc5c59
Alpha Recarga <alpharecargas0@gmail.com>
2026-05-27 05:04:30 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Navbar } from './components/Navbar'
 import { LogoAlpha } from '@/components/logo-alpha'
 import { HeroSection } from './components/sections/HeroSection'
 import { PlansSection } from './components/PlansSection'
@@ -12,8 +13,11 @@ import { FloatingWhatsApp } from './components/FloatingWhatsApp'
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#0a0e27] text-white">
+    <main className="min-h-screen bg-[#0a0e27] text-white pt-20">
       
+      {/* NAVBAR */}
+      <Navbar />
+
       {/* HERO */}
       <HeroSection />
 
